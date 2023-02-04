15e59152c85dfe9df430ce60136097302ab55d36
kiranpagar123 <124418709+kiranpagar123@users.noreply.github.com>
2023-02-04 11:06:32 +0530

    Add files via upload

---
+++ assignment1.py
@@ -0,0 +1,22 @@
+# Assignment 1
+# by Kiran pagar
+
+a=0
+b=1
+print("The Fibbonancy series between 0 to 50 is -->",end=" ")
+print(b,end=" ")
+for i in range(2,10):
+    c=a+b
+    print(c,end=" ")
+    a=b
+    b=c
+
+
+#Output -->The Fibbonancy series between 0 to 50 is --> 1 1 2 3 5 8 13 21 34    
+
+
+
+
+
+
+
