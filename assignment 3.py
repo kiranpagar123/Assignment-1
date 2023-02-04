225e0819dbdb734908d989f8168401780b9bf5bb
kiranpagar123 <124418709+kiranpagar123@users.noreply.github.com>
2023-02-04 14:40:37 +0530

    Add files via upload

---
+++ assignment 3.py
@@ -0,0 +1,28 @@
+# Sample numbers : numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9) 
+
+# Expected Output :
+
+# Number of even numbers : 4
+
+# Number of odd numbers : 5
+
+
+
+count=0
+for i in range(2,10,2):
+    count+=1
+print("Number of even numbers is -->",count)
+
+
+count=0
+for i in range(1,10,2):
+    count+=1
+print("Number of odd numbers is -->",count)
+
+
+    
+
+
+
+
+
